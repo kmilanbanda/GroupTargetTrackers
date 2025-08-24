a4162f3cc8aa8b5662f549cdd2810d50e0f2ad21
kmilanbanda <kmilanbanda@gmail.com>
2025-08-23 18:21:26 -0700

    Added Plater compatibility and basic options menu for update interval and token size

--- RainTargetTrackers.lua
+++ RainTargetTrackers.lua
@@ -1,8 +1,12 @@
 MyAddon = MyAddon or {}
+MyAddon.callbacks = {}
 
 if not MyAddonDB then
     MyAddonDB = {}
 end
+
+MyAddonDB.updateInterval = MyAddonDB.updateInterval or 0.2
+MyAddonDB.tokenSize = MyAddonDB.tokenSize or 20
 
 print("Rain Target Trackers successfully loaded!")
 if not MyAddonDB.loadCount then
@@ -10,54 +14,36 @@ if not MyAddonDB.loadCount then
 end
 MyAddonDB.loadCount = MyAddonDB.loadCount + 1
 
-local mainFrame = CreateFrame("Frame", "MyAddonMainFrame", UIParent, "BasicFrameTemplateWithInset")
-mainFrame:SetSize(500, 350)
-mainFrame:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
-mainFrame.TitleBg:SetHeight(30)
-mainFrame.title = mainFrame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
-mainFrame.title:SetPoint("TOPLEFT", mainFrame.TitleBg, "TOPLEFT", 5, -3)
-mainFrame.title:SetText("Group Priority Targeting")
-mainFrame:Hide()
-
-mainFrame:EnableMouse(true)
-mainFrame:SetMovable(true)
-mainFrame:RegisterForDrag("LeftButton")
-mainFrame:SetScript("OnDragStart", function(self)
-	self:StartMoving()
-end)
-mainFrame:SetScript("OnDragStop", function(self)
-	self:StopMovingOrSizing()
-end)
-
-mainFrame:SetScript("OnShow", function()
-    PlaySound(808)
-end)
-
-mainFrame:SetScript("OnHide", function()
-    PlaySound(808)
-end)
-
-SLASH_ADDON1 = "/rtt"
-SlashCmdList["ADDON"] = function()
-	if mainFrame:IsShown() then
-    	mainFrame:Hide()
-	else
-    	mainFrame:Show()
-	end
+local Plater = _G["Plater"]
+if Plater then
+    print("Plater API accessed successfully!")
 end
 
-table.insert(UISpecialFrames, "MyAddonMainFrame")
-
-mainFrame.playerName = mainFrame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-mainFrame.playerName:SetPoint("TOPLEFT", mainFrame, "TOPLEFT", 15, -35)
-mainFrame.playerName:SetText("Character: " .. UnitName("player"))
-mainFrame.playerName:SetText("Character: " .. UnitName("player") .. " (Level " .. UnitLevel("player") .. ")")
+SLASH_DUMP1 = "/refresh"
+SlashCmdList["DUMP"] = function()
+    RefreshTokens()
+end
 
 local circleTextures = {}
 local targetCounts = {}
 local currentTargets = {}
 
-local UpdateInterval = 0.2
+local updateInterval = MyAddonDB.updateInterval or 0.2
+local tokenSize = MyAddonDB.tokenSize or 16
+
+local function GetAtlas(unitID)
+    local role = UnitGroupRolesAssigned(unitID)
+    
+    local atlas = ""
+    if role == "TANK" then
+        atlas = "groupfinder-icon-role-large-tank"
+    elseif role == "HEALER" then
+        atlas = "groupfinder-icon-role-large-heal" -- It is critical that this says "heal" and not "healer"
+    else
+        atlas = "groupfinder-icon-role-large-dps"
+    end
+    return atlas
+end
 
 function CreateCircle(unitID)
     if not unitID then
@@ -77,21 +63,32 @@ function CreateCircle(unitID)
     end
 
     texture:SetTexture("Interface\\LFGFrame\\UI-LFG-ICON-ROLES")
-    local role = UnitGroupRolesAssigned(unitID)
-    local atlas = ""
-    if role == "TANK" then
-        atlas = "groupfinder-icon-role-large-tank"
-    elseif role == "HEALER" then
-        atlas = "groupfinder-icon-role-large-heal" -- It is critical that this says "heal" and not "healer"
-    else
-        atlas = "groupfinder-icon-role-large-dps"
-    end
-    texture:SetAtlas(atlas)
-    texture:SetSize(16,16)
+    texture:SetAtlas(GetAtlas(unitID))
+    texture:SetSize(MyAddonDB.tokenSize, MyAddonDB.tokenSize)
     circleTextures[unitID] = texture
 end
 
-function CreateTargetCount(targetNamePlate) --use GUID as key instead?
+function RefreshToken(unitID)
+    texture = circleTextures[unitID]
+
+    texture:SetTexture("Interface\\LFGFrame\\UI-LFG-ICON-ROLES")
+    texture:SetAtlas(GetAtlas(unitID))
+    if MyAddonDB.tokenSize then
+        texture:SetSize(MyAddonDB.tokenSize, MyAddonDB.tokenSize)
+    else
+        print("Error: MyAddonDB.tokenSize not initialized. Setting to defaults")
+        texture:SetSize(16, 16)
+        MyAddonDB.tokenSize =  16
+    end
+end
+
+function RefreshTokens()
+    for unit, _ in pairs(circleTextures) do
+        RefreshToken(unit)
+    end
+end
+
+function CreateTargetCount(targetNamePlate) 
 	targetCounts[targetNamePlate] = 0
 end
 
@@ -110,18 +107,16 @@ function ResetTargetCount(targetNamePlate)
 	targetCounts[targetNamePlate] = 0
 end
 
-function UpdateCountsOnTargets(targetNamePlate)
-	--loop through all eligible units targets and correct the counters?	
-end
-
-function UpdateTexture(targetNamePlate, texture) -- must replace magic numbers at some point with actual user options
-    --print("updating texture")
+function UpdateTexture(targetNamePlate, texture)
     texture:Hide()
     texture:SetParent(targetNamePlate.UnitFrame)
-    local circleOffset = (targetCounts[targetNamePlate] - 1) * 20
-    texture:SetPoint("CENTER", targetNamePlate, "CENTER", -60 + circleOffset, 20)
+    if Plater then texture:SetParent(targetNamePlate.unitFrame) end
+    local circleOffsetMultiplier = MyAddonDB.tokenSize / 16
+    local circleOffset = (targetCounts[targetNamePlate] - 1) * 20 * circleOffsetMultiplier
+    local yOffset = 20
+    if Plater then  yOffset = yOffset + 10 end
+    texture:SetPoint("CENTER", targetNamePlate, "CENTER", -60 + circleOffset, yOffset)
     texture:Show()
-    --print("texture showing")
 end
 
 function UnitHasTarget(unit)
@@ -129,9 +124,17 @@ function UnitHasTarget(unit)
 end
 
 function UpdateCircle(unit)
-    texture = circleTextures[unit]
+    local texture = circleTextures[unit]
     if UnitHasTarget(unit) then
         local targetNamePlate = C_NamePlate.GetNamePlateForUnit(unit .. "target")
+        if Plater then
+            local plates = Plater.GetAllShownPlates()
+            for _, plate in pairs(plates) do
+               if UnitGUID(unit .. "target") == UnitGUID(plate.unitFrame.unit) then
+                   targetNamePlate = plate
+               end
+            end
+        end
         if targetNamePlate then
             if not targetCounts[targetNamePlate] then
                 CreateTargetCount(targetNamePlate) 
@@ -168,67 +171,27 @@ local function IsEligibleUnit(unitID)
     return string.match(unitID, partyPattern) or string.match(unitID, raidPattern) or unitID == "player"
 end
 
-local function PrintTargetChanges(unitID)
-    print("Unit: " .. unitID)
-
-    local targetUnit = unitID .. "target"
-    
-    if MyAddonDB.settingsKeys.enableGUIDPrinting then
-        local unitGUID = UnitGUID(unitID)
-        local targetGUID = UnitGUID(targetUnit)
-        if UnitExists(targetUnit) then
-            print(unitID .. " (" .. unitGUID .. ")" .. "'s new target is: " .. UnitName(targetUnit) .. " (" .. targetGUID .. ")")
-        else
-            print(unitID .. " (" .. unitGUID .. ")" .. " has no target.")
-        end
-    else
-        if UnitExists(targetUnit) then
-            print(unitID .. "'s new target is: " .. UnitName(targetUnit))
-        else
-            print(unitID .. " has no target.")
-        end
-    end
-end
-
 local function IsUnitsTarget(unitID, targetID)
     return UnitGUID(unitID) == UnitGUID(targetID)
 end
 
 local eventListenerFrame = CreateFrame("Frame", "MyAddonEventListenerFrame", UIParent)
-local function eventHandler(self, event, unitID)
-    if event == "UNIT_TARGET" then
---        if IsEligibleUnit(unitID) then
---            if not circleTextures[unitID] then
---                print("Creating circle for " .. unitID)
---                CreateCircle(unitID)
---            end
---            UpdateCircle(unitID)
---
---            if MyAddonDB.settingsKeys.enablePrinting then
---                PrintTargetChanges(unitID)
---	        end
---        end
---
-    elseif event == "NAME_PLATE_UNIT_REMOVED" then
-        for unit, texture in pairs(circleTextures) do
-            local targetID = unit .. "target"
-            if IsUnitsTarget(unitID, targetID) then
-                --reset target count to zero
-                --targetNamePlate = currentTarget[unit]
-                --ResetTargetCount(targetNamePlate)
-                --texture:Hide()
-            end
-        end
-    elseif event == "NAME_PLATE_UNIT_ADDED" then
-        for unit, texture in pairs(circleTextures) do
-            local targetID = unit .. "target"
-            if IsUnitsTarget(unitID, targetID) then
-                --ResetTargetCount(currentTarget[unit])
-				--UpdateCircle(texture, unit)
-                --texture:Show()
-            end
-        end
+eventListenerFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+eventListenerFrame:RegisterEvent("GROUP_ROSTER_UPDATE")
+eventListenerFrame:RegisterEvent("GROUP_JOINED")
+local function eventHandler(self, event)
+    if event == "PLAYER_ENTERING_WORLD" or event == "GROUP_ROSTER_UPDATE" then
+        RefreshTokens()
     end
+end
+
+eventListenerFrame:SetScript("OnEvent", eventHandler)
+
+function OnMenuClosed(frame)
+    updateInterval = MyAddonDB.updateInterval
+    ticker:Cancel()
+    ticker = C_Timer.NewTicker(updateInterval, Update)
+    RefreshTokens()    
 end
 
 local function GetGroupType()
@@ -260,12 +223,7 @@ function Update()
         ResetTargetCount(target)
     end
 end
-local ticker = C_Timer.NewTicker(UpdateInterval, Update)
-
-eventListenerFrame:SetScript("OnEvent", eventHandler)
-eventListenerFrame:RegisterEvent("UNIT_TARGET")
-eventListenerFrame:RegisterEvent("NAME_PLATE_UNIT_REMOVED")
-eventListenerFrame:RegisterEvent("NAME_PLATE_UNIT_ADDED")
+ticker = C_Timer.NewTicker(updateInterval, Update)
 
 function MyAddon:ToggleMainFrame()
     if not mainFrame:IsShown() then
--- Settings.lua
+++ Settings.lua
@@ -1,24 +1,52 @@
-local checkboxes = 0
+MyAddonDB = MyAddonDB or {}
+MyAddon.callbacks = MyAddon.callbacks or {}
+
+MyAddonDB.updateInterval = MyAddonDB.updateInterval or 0.2
+MyAddonDB.tokenSize = MyAddonDB.tokenSize or 20
+
+local settingsHeight = 1
 local settings = {
-    {
-        settingText = "Enable event tracking",
-        settingKey = "enablePrinting",
-        settingTooltip = "While enabled, target changes will be printed to the chatbox",
+--    enablePrinting = {
+--       settingText = "Enable event tracking",
+--       settingTooltip = "While enabled, target changes will be printed to the chatbox",
+--       settingType = "checkbox",
+--       settingKey = "enablePrinting",
+--    },
+    updateInterval = {
+        settingText = "Update Interval",
+        settingTooltip = "Configure the amount of time between updates. A lower value means more frequent updates",
+        settingType = "slider",
+        settingMin = 0.1,
+        settingMax = 1,
+        settingValue = MyAddonDB.updateInterval or 0.2,
+        settingDecimals = 1,
+        settingStep =  0.1,
+        settingKey = "updateInterval",
+        settingDefault = 0.2
     },
-    {
-        settingText = "Enable GUID printing",
-        settingKey = "enableGUIDPrinting",
-        settingTooltip = "While enabled, printed messages will also add GUID tags to all unit IDs"
+    tokenSize = {
+        settingText = "Token Size",
+        settingTooltip = "Configure the size of the tokens displayed on enemy nameplates",
+        settingType = "slider",
+        settingMin = 8,
+        settingMax = 64,
+        settingValue = MyAddonDB.tokenSize or 20,
+        settingDecimals = 0,
+        settingStep = 1,
+        settingKey = "tokenSize",
+        settingDefault = 20
     },
 }
+
+MENU_CLOSED = MENU_CLOSED or "MENU_CLOSED"
 
 local settingsFrame = CreateFrame("Frame", "MyAddonSettingsFrame", UIParent, "BasicFrameTemplateWithInset")
 settingsFrame:SetSize(400, 300)
 settingsFrame:SetPoint("CENTER")
 settingsFrame.TitleBg:SetHeight(30)
 settingsFrame.title = settingsFrame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
-settingsFrame.title:SetPoint("CENTER", settingsFrame.TitleBg, "CENTER", 0, -3)
-settingsFrame.title:SetText("Rain Priority Targets Settings")
+settingsFrame.title:SetPoint("CENTER", settingsFrame.TitleBg, "CENTER", 0, 3)
+settingsFrame.title:SetText("Rain Target Tokens")
 settingsFrame:Hide()
 settingsFrame:EnableMouse(true)
 settingsFrame:SetMovable(true)
@@ -31,10 +59,97 @@ settingsFrame:SetScript("OnDragStop", function(self)
 	self:StopMovingOrSizing()
 end)
 
+SLASH_ADDON1 = "/rtt"
+SlashCmdList["ADDON"] = function()
+	if settingsFrame:IsShown() then
+    	settingsFrame:Hide()
+	else
+    	settingsFrame:Show()
+	end
+end
+
+function MyAddon:RegisterCallback(eventName, callback)
+    if not self.callbacks[eventName] then
+        self.callbacks[eventName] = {}
+    end
+    table.insert(self.callbacks[eventName], callback)
+end
+
+function MyAddon:TriggerEvent(eventName, ...)
+    if self.callbacks[eventName] then
+        for _, callback in pairs(self.callbacks[eventName]) do
+            callback(...)
+        end
+    end
+end
+
+settingsFrame:SetScript("OnHide", function(self)
+    MyAddon:TriggerEvent(MENU_CLOSED, self)
+end)
+
+MyAddon:RegisterCallback(MENU_CLOSED, OnMenuClosed)
+
+local sliderWidth = 200
+local sliderHeight = 20
+
+local function setSliderProperties(slider, min, max, value)
+    slider:SetWidth(sliderWidth)
+    slider:SetHeight(sliderHeight)
+    slider:SetMinMaxValues(min, max)
+    slider:SetValueStep(settings[slider.key].settingStep)
+    slider:SetValue(value) 
+end
+
+local function addSliderText(slider, min, max, value)
+    slider.label = slider:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+    slider.label:SetPoint("TOP", slider, "BOTTOM", 0, -5)
+    slider.label:SetText("Adjust Value: " .. format("%.1f", value))
+
+    slider.low = slider:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+    slider.low:SetPoint("TOPLEFT", slider, "BOTTOMLEFT", -5, -20)
+    slider.low:SetText(min)
+
+    slider.high = slider:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+    slider.high:SetPoint("TOPRIGHT", slider, "BOTTOMRIGHT", 5, -20)
+    slider.high:SetText(max)
+end
+
+function round(number, decimals)
+    local multiplier = 10^decimals
+    return math.floor(number * multiplier + 0.5) / multiplier
+end
+
+local function handleSliderValueChanges(slider)
+    slider:SetScript("OnValueChanged", function(self, value)
+        local roundedValue = round(value, settings[slider.key].settingDecimals) 
+        MyAddonDB[slider.key] = roundedValue
+        slider.label:SetText("Adjust Value: " .. format("%.1f", roundedValue))
+    end)
+end
+
+local function CreateSlider(sliderText, key, tooltip, min, max)
+    local slider = CreateFrame("Slider", "MyAddonSlider" .. sliderText, settingsFrame, "OptionsSliderTemplate")
+    slider.Text:SetText(sliderText)
+    slider:SetPoint("CENTER", settingsFrame, "TOP", 0, -30 + (settingsHeight * -30))
+    slider.key = key
+
+    if MyAddonDB.settingsKeys[key] == nil then
+        MyAddonDB.settingsKeys[key] = true
+    end
+    
+    value = MyAddonDB[key] or settings[key].settingDefault
+    setSliderProperties(slider, min, max, value)
+    addSliderText(slider, min, max, value)
+    handleSliderValueChanges(slider)
+    
+    settingsHeight = settingsHeight + 2.5
+    return slider
+end
+
 local function CreateCheckbox(checkboxText, key, checkboxTooltip)
-    local checkbox = CreateFrame("CheckButton", "MyAddonCheckboxID" .. checkboxes, settingsFrame, "UICheckButtonTemplate")
+    local checkbox = CreateFrame("CheckButton", "MyAddonCheckboxID" .. settingsHeight, settingsFrame, "UICheckButtonTemplate")
     checkbox.Text:SetText(checkboxText)
-    checkbox:SetPoint("TOPLEFT", settingsFrame, "TOPLEFT", 10, -30 + (checkboxes * -30))
+    checkbox:SetPoint("TOPLEFT", settingsFrame, "TOPLEFT", 10, -30 + (settingsHeight * -30))
 
     if MyAddonDB.settingsKeys[key] == nil  then
         MyAddonDB.settingsKeys[key] = true
@@ -55,7 +170,7 @@ local function CreateCheckbox(checkboxText, key, checkboxTooltip)
         MyAddonDB.settingsKeys[key] = self:GetChecked()
     end)
 
-    checkboxes = checkboxes + 1
+    settingsHeight = settingsHeight + 2
 
     return checkbox
 end
@@ -63,7 +178,6 @@ end
 local eventListenerFrame = CreateFrame("Frame", "MyAddonSettingsEventListenerFrame", UIParent)
 
 eventListenerFrame:RegisterEvent("PLAYER_LOGIN")
-
 eventListenerFrame:SetScript("OnEvent", function(self, event)
     if event == "PLAYER_LOGIN" then
         if not MyAddonDB.settingsKeys then
@@ -71,7 +185,13 @@ eventListenerFrame:SetScript("OnEvent", function(self, event)
         end
 
         for _, setting in pairs(settings) do
-            CreateCheckbox(setting.settingText, setting.settingKey, setting.settingTooltip)
+            if setting.settingType == "checkbox" then
+                CreateCheckbox(setting.settingText, setting.settingKey, setting.settingTooltip)
+            elseif setting.settingType == "slider" then
+                CreateSlider(setting.settingText, setting.settingKey, setting.settingTooltip, setting.settingMin, setting.settingMax)
+            else
+                print("Rain Target Trackers: invalid setting type")
+            end
         end
     end
 end)
